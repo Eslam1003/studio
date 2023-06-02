70971c2235c131125e8c52aaf4bc2cff0bfb78d7
islam <eslam.ramadan1003@gmail.com>
2023-06-02 16:31:15 +0200

    Done with the login code

--- studio/Login.cs
+++ studio/Login.cs
@@ -17,27 +17,35 @@ namespace studio
         {
             InitializeComponent();
             con = new DB();
-            
         }
 
-        private string UserName, Password;
+        private string UserName, Password,count;
         DataTable users ;
+
         public void ShowData()
         {
             string Query = $"SELECT * FROM users where user_name='{UserName}'AND password='{Password}'";
             users = con.GetData(Query);
-            dataGridView1.DataSource = users;
+            count = Convert.ToString(users.Rows.Count);
         }
 
         private void LoginBtn_Click(object sender, EventArgs e)
         {
             UserName = Login_UserName.Text;
             Password = Login_UserName.Text;
-           
-           
-           // Main main = new Main();
-            //main.Show();
-            //this.Hide();
+            ShowData();
+            if (count == "0")
+            {
+                MessageBox.Show("Not Valed User Name");
+            }else if(count == "1")
+            {
+                 Main main = new Main();
+                main.Show();
+                this.Hide();
+            }
+
+
+
         }
 
         private void UserNameText_TextChanged(object sender, EventArgs e)
